1686ef988da3f0b9e6bbf6ded2ce1d1a160b9717
dibbodas12 <dibbodas228@gmail.com>
2025-03-01 18:35:14 +0600

    2460 updated

--- Array/2460. Apply Operations to an Array.cpp
+++ Array/2460. Apply Operations to an Array.cpp
@@ -1,5 +1,6 @@
 //* Submitted at Mar 01, 2025 10:14
-//? TC --> O(N) SC --> O(1)
+//? TC --> O(N) SC --> O(N)
+// 3 loop
 class Solution {
     public:
         vector<int> applyOperations(vector<int>& nums) {
@@ -23,3 +24,59 @@ class Solution {
             return ans;
         }
     };
+
+    //? TC --> O(N) SC --> O(1)
+    // 2 loop 
+    class Solution {
+        public:
+            vector<int> applyOperations(vector<int>& nums) {
+                int n = nums.size();
+                for(int i=0; i<n-1; i++)
+                {
+                    if(nums[i] == nums[i+1])
+                    {
+                        nums[i] *= 2;
+                        nums[i+1] = 0;
+                    }
+                }
+                int i=0,j=0;
+                while(i < n)
+                {
+                    if(nums[i] != 0)
+                    {
+                        swap(nums[j],nums[i]);
+                        j++;
+                    }
+                    i++;
+                }
+                return nums;
+            }
+        };
+
+    
+
+    //? TC --> O(N) SC --> O(1)
+    // one loop 1 pass solution            
+        class Solution {
+            public:
+                vector<int> applyOperations(vector<int>& nums) {
+                    int n = nums.size();
+                    int j = 0;
+                    for(int i=0; i<n; i++)
+                    {
+                        if( i+1 < n and nums[i] == nums[i+1])
+                        {
+                            nums[i] *= 2;
+                            nums[i+1] = 0;
+                        }
+                        if(nums[i] != 0)
+                        {
+                            if(j != i)  swap(nums[i],nums[j]);
+                            j++;
+                        }
+            
+                    }
+                    return nums;
+                    
+                }
+            };

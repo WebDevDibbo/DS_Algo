d2ce1f656226a7a97743432b89dd4e5aa6394f70
dibbodas12 <dibbodas228@gmail.com>
2025-03-12 10:14:07 +0600

    2529 solved binary search

---
+++ Array/2529. Maximum Count of Positive Integer and Negative Integer.cpp
@@ -0,0 +1,13 @@
+//* TC --> o(logn)   S.C : O(1)  (Using lowerbound)
+class Solution {
+    public:
+        int maximumCount(vector<int>& nums) {
+            int n = nums.size();
+    
+            int firstP = lower_bound(begin(nums), end(nums), 1) - begin(nums);
+            int firstN = lower_bound(begin(nums), end(nums), 0) - begin(nums);
+    
+            return max(n-firstP, firstN);
+            
+        }
+    };
